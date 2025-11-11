32a6ba116f0029eb096a806720245eb86a31ac51
AliKe <kenesbekovalmuhammed7@gmail.com>
2025-11-12 04:04:07 +0500

    Attribs class has unique set of attribs.

---
+++ Attributes.py
@@ -0,0 +1,7 @@
+class Attributes:
+    def __init__(self, combat, vigor, mobility, charisma, intellect):
+        self.combat = combat
+        self.vigor = vigor
+        self.mobility = mobility
+        self.charisma = charisma
+        self.intellect = intellect
